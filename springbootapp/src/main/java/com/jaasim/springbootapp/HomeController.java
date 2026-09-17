b67cdf37d154ce34c4afa90044248c5487e5d845
jaasimahamad <jaasimahamad.m2025aids@sece.ac.in>
2026-09-17 09:23:07 +0530

    Initialize Spring Boot application with basic structure and configuration files

---
+++ springbootapp/src/main/java/com/jaasim/springbootapp/HomeController.java
@@ -0,0 +1,31 @@
+package com.jaasim.springbootapp;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("")
+    public String get() {
+        return "Hello World";
+    }
+    @PostMapping("create")
+    public String postMethodName(@RequestBody String payload) {
+        return payload+ "Successfully created";
+    }
+    @DeleteMapping("delete/{id}")
+    public String delete(@PathVariable String id) {
+        return "deleted "+id;
+    }
+    @getMapping("get/{id}")
+    public String getById(@PathVariable String id) {
+        return "get "+id;
+    }
+}
